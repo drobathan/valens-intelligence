7767fdfad9a7281823551b4f3fcff6f887589823
Dan Robathan <danrobathan@Dans-MacBook-Pro-2.local>
2026-03-23 14:14:13 +0000

    fix: remove clerk from middleware, handle auth client-side

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,10 @@
-export const runtime = 'nodejs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/api/webhooks(.*)',
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
